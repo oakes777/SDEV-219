684eedb519c8912d32ff17b2f438ef5aa2b01b2d
oakes777 <sule.jonathan@student.greenriver.edu>
2024-01-24 11:25:07 -0800

    removed a println statement

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Apple apple=new Apple("Red Delicious", "Washington State", true);
         System.out.println(apple);
-        System.out.println(apple.getGrownAt());
     }
 }
